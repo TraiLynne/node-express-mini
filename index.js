73c0bda904a8353d997ecdf8b1491463c941a933
Trai Lynne <trailynne@gmail.com>
2019-01-12 19:27:14 -0500

    Built Seven deadly sins GET API endpoint

--- index.js
+++ index.js
@@ -2,11 +2,52 @@
 const express = require('express');
 
 const server = express();
+const port = 4000;
 
 server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+server.get('/sins', (req, res) => {
+    const sins = [
+        {
+            id: 1,
+            name: 'Meliodas'
+        },
+        {
+            id: 2,
+            name: 'Liz'
+        },
+        {
+            id: 3,
+            name: 'Diana'
+        },
+        {
+            id: 4,
+            name: 'Ban'
+        },
+        {
+            id: 5,
+            name: 'King'
+        },
+        {
+            id: 6,
+            name: 'Gowther'
+        },
+        {
+            id: 7,
+            name: 'Merlin'
+        },
+        {
+            id: 8,
+            name: 'Escobar'
+        }
+    ];
+
+    res
+        .status(200)
+        .json(sins);
+});
 
 
-server.listen(4000, () => console.log('API running on port 4000'));
+server.listen(port, () => console.log('API running on port 4000'));
