3ee78ad9d46549ad7849229bf6481a5514c81f66
Trai Lynne <trailynne@gmail.com>
2019-01-16 12:05:37 -0500

    Copied code over from Lecture follow along

--- index.js
+++ index.js
@@ -1,86 +1,133 @@
-// implement your API here
+// const http = require('http'); // Built in with node.js to handle http traffic
 const express = require('express');
-
 const server = express();
-const port = 4000;
 
-const db = require('./data/db')
+const port = 4000; // Port watching for traffic
 
-server.use(express.json())
+const db = require('./data/db');
+
+server.use(express.json());
+
+const sins = [{
+        id: 0,
+        name: 'Meliodas'
+    },
+    {
+        id: 1,
+        name: 'Liz'
+    },
+    {
+        id: 2,
+        name: 'Diana'
+    },
+    {
+        id: 3,
+        name: 'Ban'
+    },
+    {
+        id: 4,
+        name: 'King'
+    },
+    {
+        id: 5,
+        name: 'Gowther'
+    },
+    {
+        id: 6,
+        name: 'Merlin'
+    },
+    {
+        id: 7,
+        name: 'Escobar'
+    }
+];
+
+let currentId = sins[sins.length - 1].id;
 
 server.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World, from Express')
 });
+
 // C - Create
 server.post('/sins', (req, res) => {
-    
+    console.log(req.body)
+    const sin = req.body;
+    sin.id = ++currentId;
+
+    sins.push(sin);
+
     res
         .status(201)
-        .json({
-            url: '/sins',
-            operation: 'POST'
-        });
+        .json(sins);
 });
 
 // R - Read
 server.get('/sins', (req, res) => {
-    const sins = [
-        {
-            id: 1,
-            name: 'Meliodas'
-        },
-        {
-            id: 2,
-            name: 'Liz'
-        },
-        {
-            id: 3,
-            name: 'Diana'
-        },
-        {
-            id: 4,
-            name: 'Ban'
-        },
-        {
-            id: 5,
-            name: 'King'
-        },
-        {
-            id: 6,
-            name: 'Gowther'
-        },
-        {
-            id: 7,
-            name: 'Merlin'
-        },
-        {
-            id: 8,
-            name: 'Escobar'
-        }
-    ];
+    // query string parameters get added to req.query
+    const sortField = req.query.sortby || 'id';
+    // apply the sorting
+    const sortedSins = sins.sort(
+        (a, b) => (a[sortField] < b[sortField] ? -1 : 1)
+    );
 
     res
         .status(200)
-        .json(sins);
+        .json(sortedSins);
 });
 
-// U - Update 
-server.put('/hobbits', (req, res) => {
-    res
-        .status(200)
-        .json({
-            url: '/sins',
-            operation: 'PUT'
-        });
+// U - Update
+server.put('/sins/:id', (req, res) => {
+    const sin = sins.find(s => s.id == req.params.id); //string == number not string === number
+
+    if (!sin) {
+        res
+            .status(404)
+            .json({
+                message: 'Sin does not exist'
+            });
+    } else {
+        // modify the existing hobbit
+        Object.assign(sin, req.body);
+
+        res
+            .status(200)
+            .json(sin);
+    }
 });
 
 // D - Destroy
-server.delete('/hobbits', (req, res) => {
-    res.status(204);
+server.delete('/sins/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.params);
+    // or we could destructure it like so: const { id } = req.params;
+    res
+        .status(200)
+        .json({
+            url: `/sins/${id}`,
+            opeation: `DELETE for hobbit with id ${id}`,
+        });
 });
 
 
 
+
+
+// *** ==== USERS PORTION ==== *** //
+
+// C - Create
+server.post('/sins', (req, res) => {
+    console.log(req.body)
+    const sin = req.body;
+    sin.id = ++currentId;
+
+    sins.push(sin);
+
+    res
+        .status(201)
+        .json(sins);
+});
+
+// R - READ
 server.get('/users', (req, res) => {
     db
         .find()
@@ -91,5 +138,7 @@ server.get('/users', (req, res) => {
             res.status(500).send('Error Occurred. Please Try Again')
         });
 });
+// U - UPDATE
+// D - DESTROY
 
 server.listen(port, () => console.log('API running on port 4000'));
